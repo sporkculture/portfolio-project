452acd24b1ad07fe54051c0f26d35e62c6b98a89
Jesse Acklin <jackins@gmail.com>
2018-04-16 05:02:50 -0400

    Major progress on frontend

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+	title = models.CharField(max_length=250)
+	publish_date = models.DateField( auto_now_add=True)
+	body = models.TextField()
+	image = models.ImageField(upload_to='images/')
